530b00fb5227694b95171d845c10b4d76f49af46
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-03-09 10:54:05 -0800

    Updates

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,19 @@
-$(".option").click(function(){
+$(".option").click(function () {
    $(".option").removeClass("active");
    $(this).addClass("active");
-   
 });
+
+/*
+<div class="option" style="--optionBackground:url(BACKGROUND IMAGE URL);">
+      <div class="shadow"></div>
+      <div class="label">
+         <div class="icon">
+   <svg>ICON FROM ASSETS</svg>
+         </div>
+         <div class="info">
+            <div class="main">TITTLE</div>
+            <div class="sub">SUBTEXT</div>
+         </div>
+      </div>
+   </div>
+*/
